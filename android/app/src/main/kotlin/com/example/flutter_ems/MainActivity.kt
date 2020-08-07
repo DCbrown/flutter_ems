d13c97f685d6633589b98c2e64666562ec8264de
donovanbrown <browncdonovan@gmail.com>
2020-08-07 19:38:35 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_ems/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ems
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import '../screens/home/home.dart';
+import 'package:flutter/material.dart';
+import '../screens/authenticate/authenticate.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
